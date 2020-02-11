e6a9d7908e212725b7fa224f324c3c1e74ea7b76
bharatcorporatetrainer <bharatcorporatetrainer@gmail.com>
2020-02-11 10:14:49 +0530

    .

--- src/test/java/testcases/mytheresa/Test1.java
+++ src/test/java/testcases/mytheresa/Test1.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -25,7 +26,9 @@ public class Test1 {
   public void test1() throws Exception {
     //driver.get(baseUrl + "/?gfe_rd=cr&ei=4rKzWKGgC7TT8gfDgZzoCA&gws_rd=ssl");
     
-    if (driver.getPageSource().contains("testbharat")) {
+	WebElement uname=  driver.findElement(By.name("test"));
+	uname.sendKeys("Bharat");
+    if (driver.getPageSource().contains("interview Questions")) {
 		System.out.println("Yes test exist");
 	}
 
